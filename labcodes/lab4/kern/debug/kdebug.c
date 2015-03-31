14c499d01c265916a8a679d60f38fa4da13ae4b4
moreD <moreDatPublic@gmail.com>
2015-03-31 16:39:09 +0800

    lab4 patch & code

--- labcodes/lab4/kern/debug/kdebug.c
+++ labcodes/lab4/kern/debug/kdebug.c
@@ -293,7 +293,7 @@ read_eip(void) {
  * */
 void
 print_stackframe(void) {
-     /* LAB1 YOUR CODE : STEP 1 */
+     /* LAB1 2013011407 : STEP 1 */
      /* (1) call read_ebp() to get the value of ebp. the type is (uint32_t);
       * (2) call read_eip() to get the value of eip. the type is (uint32_t);
       * (3) from 0 .. STACKFRAME_DEPTH
@@ -305,5 +305,20 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+	uint32_t ebp = read_ebp();
+	uint32_t eip = read_eip();
+
+	for (; ebp != 0;) {
+		cprintf("ebp:0x%08x eip:0x%08x ", ebp, eip);
+		int i;
+		uint32_t *args;
+		args = (uint32_t *)ebp + 2;
+		cprintf("args:");
+		for (i = 0; i < 4; ++i) cprintf("0x%08x ", args[i]);
+		cprintf("\n");
+		print_debuginfo(eip - 1);
+		eip = *((uint32_t *)ebp + 1);
+		ebp = *((uint32_t *)ebp);
+	}
 }
 
