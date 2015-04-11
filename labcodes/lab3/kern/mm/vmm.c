3504f891354f74acc1dbccc635a6c8fafbad9dd0
moreD <moreDatPublic@gmail.com>
2015-04-11 19:23:57 +0800

    lab3 report

--- labcodes/lab3/kern/mm/vmm.c
+++ labcodes/lab3/kern/mm/vmm.c
@@ -355,10 +355,6 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
 			swap_in(mm, addr, &page);
 			page_insert(mm->pgdir, page, addr, perm);
 			swap_map_swappable(mm, addr, page, 1);
-                                    //(1）According to the mm AND addr, try to load the content of right disk page
-                                    //    into the memory which page managed.
-                                    //(2) According to the mm, addr AND page, setup the map of phy addr <---> logical addr
-                                    //(3) make the page swappable.
         }
         else {
             cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
